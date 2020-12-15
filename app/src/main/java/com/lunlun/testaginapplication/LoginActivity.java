cb4a05f7e5d7b4442f9bf12b69570dea0a8c8215
minlun0518 <hello10050067@gmail.com>
2020-12-16 01:09:33 +0800

    連上資料庫囉

--- app/src/main/java/com/lunlun/testaginapplication/LoginActivity.java
+++ app/src/main/java/com/lunlun/testaginapplication/LoginActivity.java
@@ -16,14 +16,17 @@ import android.content.pm.PackageManager;
 import android.hardware.fingerprint.FingerprintManager;
 import android.os.Bundle;
 import android.os.CancellationSignal;
+import android.os.Handler;
 import android.telephony.TelephonyManager;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,8 +34,20 @@ import com.google.android.gms.tasks.OnCanceledListener;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
+
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -51,6 +66,13 @@ public class LoginActivity extends AppCompatActivity {
     private FingerprintManager mFingerprintManager;
     private CancellationSignal cancellationSignal;
 
+    //蓉蓉
+    TextInputEditText textInputEditTextIDorEmail, textInputEditTextPassword;
+    Button buttonLogin;
+    TextView textViewSignUp;
+    ProgressBar progressBar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,12 +81,77 @@ public class LoginActivity extends AppCompatActivity {
 
         getImei();//得到IMEI
 //        checkDivicd();
-        findView();
+//        findView();
 
-        email = findViewById(R.id.ed_email);
-        eemail = email.getText().toString();
-        password = findViewById(R.id.ed_password);
-        pass = password.getText().toString();
+//        email = findViewById(R.id.ed_email);
+//        eemail = email.getText().toString();
+//        password = findViewById(R.id.ed_password);
+//        pass = password.getText().toString();
+
+        textInputEditTextIDorEmail = findViewById(R.id.IDorEmail);
+        textInputEditTextPassword = findViewById(R.id.password);
+        buttonLogin = findViewById(R.id.buttonLogin);
+        textViewSignUp = findViewById(R.id.signUpText);
+        progressBar = findViewById(R.id.progress);
+
+        handleSSLHandshake();
+
+        textViewSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String  email, password;
+                email = String.valueOf(textInputEditTextIDorEmail.getText());
+                password = String.valueOf(textInputEditTextPassword.getText());
+
+                if (!email.equals("") && !password.equals("")) {
+                    //Start ProgressBar first (Set visibility VISIBLE)
+                    progressBar.setVisibility(View.VISIBLE);
+                    Handler handler = new Handler();
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            //Starting Write and Read data with URL
+                            //Creating array for parameters
+                            String[] field = new String[2];
+                            field[0] = "email";
+                            field[1] = "password";
+                            //Creating array for data
+                            String[] data = new String[2];
+                            data[0] = email;
+                            data[1] = password;
+                            PutData putData = new PutData("http://192.168.137.1/Hospital/login.php", "POST", field, data); //網址要改成自己的php檔位置及自己的ip
+                            if (putData.startPut()) {
+                                if (putData.onComplete()) {
+                                    progressBar.setVisibility(View.GONE);
+                                    String result = putData.getResult();
+                                    if (result.equals("Login Success")) {
+                                        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
+//                                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+//                                        startActivity(intent);
+//                                        finish();
+                                        verifiedsuccessfully();
+                                    } else {
+                                        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            }
+                        }
+                    });
+//                    verifiedsuccessfully();
+                } else {
+                    Toast.makeText(getApplicationContext(), "All fields require", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
 //    private void checkDivicd() {
@@ -90,31 +177,22 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        //自動輸入帳號密碼
-        findViewById(R.id.soonImageButton).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                email.setText("wubetty2012@gmail.com");
-                password.setText("123456");
-            }
-        });
-
         //顯示或隱藏密碼
-        cb = findViewById(R.id.showpswcheckBox);
-        cb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                // TODO Auto-generated method stub
-                if (isChecked) {
-                    //顯示密碼
-                    password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
-                } else {
-                    //隱藏密碼
-                    password.setTransformationMethod(PasswordTransformationMethod.getInstance());
-
-                }
-            }
-        });
+////        cb = findViewById(R.id.showpswcheckBox);
+//        cb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                // TODO Auto-generated method stub
+//                if (isChecked) {
+//                    //顯示密碼
+//                    password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+//                } else {
+//                    //隱藏密碼
+//                    password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+//
+//                }
+//            }
+//        });
     }
 
     private void startFingerprintListening() {
@@ -164,7 +242,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     public void onPause() {
         super.onPause();
-        cancellationSignal.cancel();
+//        cancellationSignal.cancel();
         cancellationSignal = null;
     }
 
@@ -237,6 +315,35 @@ public class LoginActivity extends AppCompatActivity {
 //                }
 //            }
 //        });
+    }
+
+    public static void handleSSLHandshake() {
+        try {
+            TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+                public X509Certificate[] getAcceptedIssuers() {
+                    return new X509Certificate[0];
+                }
+
+                @Override
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                }
+
+                @Override
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                }
+            }};
+
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null, trustAllCerts, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            });
+        } catch (Exception ignored) {
+        }
     }
 
 //    private void register(String eemail, String pass) {
--- app/src/main/java/com/lunlun/testaginapplication/SignupActivity.java
+++ app/src/main/java/com/lunlun/testaginapplication/SignupActivity.java
@@ -5,86 +5,146 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
+
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 public class SignupActivity extends AppCompatActivity {
-    FirebaseAuth auth;
-    FirebaseAuth.AuthStateListener authStateListener;
+    TextInputEditText textInputEditTextEmployeeId, textInputEditTextPassword, textInputEditTextPassword2, textInputEditTextEmail;
+    Button buttonSignUp;
+    TextView textViewLogin;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        findview();
+        textInputEditTextEmployeeId = findViewById(R.id.employeeId);
+        textInputEditTextEmail = findViewById(R.id.email);
+        textInputEditTextPassword = findViewById(R.id.password);
+        textInputEditTextPassword2 = findViewById(R.id.password2);
+        buttonSignUp = findViewById(R.id.buttonSignUp);
+        textViewLogin = findViewById(R.id.loginText);
+        progressBar = findViewById(R.id.progress);
+
+        handleSSLHandshake(); //呼叫忽略https的證書校驗方法
+
+        textViewLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        buttonSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String employee_id, email, password, password2;
+                employee_id = String.valueOf(textInputEditTextEmployeeId.getText());
+                email = String.valueOf(textInputEditTextEmail.getText());
+                password = String.valueOf(textInputEditTextPassword.getText());
+                password2 = String.valueOf(textInputEditTextPassword2.getText());
+
+                if (!employee_id.equals("") && !email.equals("") && !password.equals("") && !password2.equals("")) {
+                    progressBar.setVisibility(View.VISIBLE);
+                    Handler handler = new Handler();
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            //Starting Write and Read data with URL
+                            //Creating array for parameters
+                            String[] field = new String[4];
+                            field[0] = "employee_id";
+                            field[1] = "email";
+                            field[2] = "password";
+                            field[3] = "password2";
+                            //Creating array for data
+                            String[] data = new String[4];
+                            data[0] = employee_id;
+                            data[1] = email;
+                            data[2] = password;
+                            data[3] = password2;
+                            PutData putData = new PutData("https://192.168.137.1/Hospital/signup.php", "POST", field, data); //網址要改成自己的php檔位置及自己的ip
+                            if (putData.startPut()) {
+                                if (putData.onComplete()) {
+                                    progressBar.setVisibility(View.GONE);
+                                    String result = putData.getResult();
+                                    if (result.equals("Sign Up Success")) {
+                                        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
+                                        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    } else {
+                                        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            }
+                        }
+                    });
+                }else {
+                    Toast.makeText(getApplicationContext(),"All fields require", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
     }
 
-    private void findview() {
-//        EditText edmail = findViewById(R.id.ed_email2);
-//        EditText edmemberId = findViewById(R.id.edmemberId);
-//        EditText edpassword = findViewById(R.id.ed_password2);
-//        EditText edagpassword = findViewById(R.id.agained_password);
+    //忽略https的證書校驗
+    public static void handleSSLHandshake() {
+        try {
+            TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+                public X509Certificate[] getAcceptedIssuers() {
+                    return new X509Certificate[0];
+                }
 
-//        String umail=edmail.getText().toString();
-        String umail="hello10050067@gmail.com";
-//        String uid=edmemberId.getText().toString();
-//        String upass=edpassword.getText().toString();
-        String upass="000000";
-//        String uspass=edagpassword.getText().toString();
+                @Override
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                }
 
-//        findViewById(R.id.singinButton).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Log.d("AUTH",umail+"/"+upass);
-//                auth.signInWithEmailAndPassword(umail,upass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<AuthResult> task) {
-//                        if(!task.isSuccessful()){
-//                            Log.d("ONComplete","登入失敗");
-//                            register(umail,upass);
-//                        }
-//                    }
-//                });
-//
-//            }
-//        });
+                @Override
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                }
+            }};
+
+            SSLContext sc = SSLContext.getInstance("TLS");
+            // trustAllCerts信任所有的證書
+            sc.init(null, trustAllCerts, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            });
+        } catch (Exception ignored) {
+        }
 
     }
-
-//    private void register(String umail, String upass) {
-//        new AlertDialog.Builder(SignupActivity.this)
-//                .setTitle("登入問題")
-//                .setMessage("無此帳號，需要註冊")
-//                .setPositiveButton("好",
-//                        new DialogInterface.OnClickListener(){
-//                            @Override
-//                            public void onClick (DialogInterface dialog, int which){
-//                                creatUser(umail,upass);
-//                            }
-//                        })
-//                .setNeutralButton("取消",null)
-//                .show();
-//    }
-
-//    private void creatUser(String umail, String upass) {
-//        auth.createUserWithEmailAndPassword(umail,upass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-//            @Override
-//            public void onComplete(@NonNull Task<AuthResult> task) {
-//                String message = task.isComplete() ? "註冊成功" : "註冊失敗" ;
-//                new AlertDialog.Builder(SignupActivity.this)
-//                        .setMessage(message)
-//                        .setNeutralButton("OK",null)
-//                        .show();
-//            }
-//        });
-//
-//    }
 }
